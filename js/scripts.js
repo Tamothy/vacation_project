1589846263d7311a1bd36a557186b3ea81acdd00
Tam Nguyen <mr.tamron@gmail.com>
2016-06-10 18:14:52 -0700

    add branching to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ $(document).ready(function () {
   $("#blanks form").submit(function (event) {
     var nameInput = $("input#name").val();
     var flowerInput = $("select#flower").val();
-    var winterInput = $("select#winter_sport").val();
+    var snowInput = $("select#snow").val();
     var seasonInput = $("input:radio[name=season]:checked").val();
 
     $(".name").text(nameInput);
 
-    if (seasonInput === "summer") {
+    if (flowerInput === "yes" && seasonInput === "fall" || seasonInput === "spring") {
+      $("div#netherlands").fadeIn();
+    } else if (snowInput === "yes" && seasonInput === "winter" || seasonInput === "winter") {
+      $("div#switzerland").fadeIn();
+    } else if (seasonInput === "summer") {
       $("div#hawaii").fadeIn();
     } else if (seasonInput === "fall") {
       $("div#france").fadeIn();
-    } else if (flowerInput === "yes" && seasonInput === "fall") {
-      $("div#netherlands").fadeIn();
-    } else if (winterInput === "yes" || seasonInput === "winter") {
-      $("div#switzerland").fadeIn();
     }
 
     event.preventDefault();
