5fdb9f38551f9915649a28d56cf05e17a31d42fb
Tam Nguyen <mr.tamron@gmail.com>
2016-06-10 17:08:07 -0700

    add vacation effects to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
   $("#blanks form").submit(function (event) {
     var nameInput = $("input#name").val();
-    var ageInput = $("input#age").val();
     var flowerInput = $("select#flower").val();
     var winterInput = $("select#winter_sport").val();
-    var seasonInput = $(input:radio[name=season]:checked).val();
+    var seasonInput = $("input:radio[name=season]:checked").val();
 
     $(".name").text(nameInput);
-    $(".age").text(foodInput);
-    $(".flower").text(flowerInput);
-    $(".winter_sport").text(winterInput);
 
-    //$("#colorLink").attr("href", "http://www.color-hex.com/color/" + colorInput.slice(1));
-
-    $("#results").show();
+    if (seasonInput === "summer") {
+      $("div#hawaii").fadeIn();
+    } else if (seasonInput === "fall") {
+      $("div#france").fadeIn();
+    } else if (flowerInput === "yes" && seasonInput === "fall") {
+      $("div#netherlands").fadeIn();
+    } else if (winterInput === "yes" || seasonInput === "winter") {
+      $("div#switzerland").fadeIn();
+    }
 
     event.preventDefault();
   });
-
 });
