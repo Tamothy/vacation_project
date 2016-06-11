4f1362579360948ae9bb5efb58ddf58d0930f079
Tam Nguyen <mr.tamron@gmail.com>
2016-06-10 18:48:15 -0700

    create hide effects in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,23 @@ $(document).ready(function () {
 
     if (flowerInput === "yes" && seasonInput === "fall" || seasonInput === "spring") {
       $("div#netherlands").fadeIn();
+      $("div#switzerland").hide();
+      $("div#hawaii").hide();
+      $("div#france").hide();
     } else if (snowInput === "yes" && seasonInput === "winter" || seasonInput === "winter") {
+      $("div#netherlands").hide();
       $("div#switzerland").fadeIn();
+      $("div#hawaii").hide();
+      $("div#france").hide();
     } else if (seasonInput === "summer") {
+      $("div#netherlands").hide();
+      $("div#switzerland").hide();
       $("div#hawaii").fadeIn();
+      $("div#france").hide();
     } else if (seasonInput === "fall") {
+      $("div#netherlands").hide();
+      $("div#switzerland").hide();
+      $("div#hawaii").hide();
       $("div#france").fadeIn();
     }
 
